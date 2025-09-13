f759b823b9c5d29d5c078e08e243beb282fe7e19
Meredith Wanta <mewanta@icloud.com>
2025-09-13 13:23:32 -0400

    Update AllText.java

--- src/main/TextEditor/AllText.java
+++ src/main/TextEditor/AllText.java
@@ -1,4 +1,42 @@
 package TextEditor;
 
+import javalib.worldimages.WorldImage;
+
+/**
+ * Represents the type of text that the text editor can process. These include body,
+ * header, and notes.
+ */
 public interface AllText {
+
+  /**
+   * Draws this text onto a background of choice.
+   *
+   * @param cursorPos : the current position of the cursor.
+   * @return the worldImage with this text visual displayed on it.
+   */
+  WorldImage draw(int cursorPos);
+
+  /**
+   * Adds the typed key one space before the cursor.
+   *
+   * @param key : the typed key.
+   * @param cursorPos : the current position of the cursor.
+   * @return a new AllText object with character added to the text before the cursor.
+   */
+  AllText type(String key, int cursorPos);
+
+  /**
+   * Removes the character one space before the cursor.
+   *
+   * @param cursorPos : the current position of the cursor.
+   * @return a new AllText object with character removed from the text before the cursor.
+   */
+  AllText backspace(int cursorPos);
+
+  /**
+   * Returns the length of this text.
+   *
+   * @return the length as an integer of this text.
+   */
+  int getLength();
 }
