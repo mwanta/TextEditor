2869527d0593983b807ff5f4fcc62c1942de9404
Meredith Wanta <mewanta@icloud.com>
2025-09-13 13:25:28 -0400

    Update TextWorldTest.java

--- src/test/TextWorldTest.java
+++ src/test/TextWorldTest.java
@@ -1,4 +1,19 @@
-package PACKAGE_NAME;
+import TextEditor.BodyText;
+import TextEditor.TextWorld;
+import org.junit.Test;
 
+/**
+ * To test and run TextWorld.
+ */
 public class TextWorldTest {
+
+  // calls big bang to launch the text editor
+  @Test
+  public void testBigBang() {
+    TextWorld world = new TextWorld(new BodyText(), 0);
+    int worldWidth = 600;
+    int worldHeight = 400;
+    double tickRate = .1;
+    world.bigBang(worldWidth, worldHeight, tickRate);
+  }
 }
